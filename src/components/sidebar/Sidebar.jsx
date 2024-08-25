4be753151eb21713ea1c51c0d6aaed877bb11fc4
eshban14 <eshban.younis@gmail.com>
2024-08-26 03:58:54 +0400

    first commit

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+
+import './Sidebar.css'
+
+const sidebar = () => {
+    return(
+        <div className="sidebar">
+            <h1>SideBar</h1>
+        </div>
+    )
+}
+
+export default sidebar
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,14 @@
+
+import './Topbar.css'
+
+const topbar = () => {
+
+    return(
+        <div className="topbar">
+
+        </div>
+    )
+
+}
+
+export default topbar;
