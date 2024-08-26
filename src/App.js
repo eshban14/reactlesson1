c4946007b1c2475608e617c3c58486cb8e86dd77
eshban14 <eshban.younis@gmail.com>
2024-08-26 15:20:52 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@
 import './App.css';
 import Topbar from './components/topbar/Topbar';
 import Sidebar from './components/sidebar/Sidebar';
+import Main from './components/main/Main';
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
     <div className="App">
       <Topbar/>
       <Sidebar/>
+      <Main/>
+      <Footer/>
     </div>
 
   );
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,14 @@
+
+
+import './Main.css'
+
+const main = () => {
+    return(
+        <div className="main">
+            <h1>Main</h1>
+
+        </div>
+    )
+}
+
+export default main
